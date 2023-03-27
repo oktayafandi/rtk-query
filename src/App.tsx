6bac0cd9d19a1b751b21f54a2eabeb4786ccb29f
Oktay Afandi <ogtayaa@code.edu.az>
2023-03-27 14:44:53 +0400

    feats

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import { Table } from "antd";
 import { useGetUsersQuery } from "./api/apiSlice";
 import User from "./types/User";
 
+const column = [
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+  },
+];
+
 function App() {
-  const { data } = useGetUsersQuery();
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery();
 
-  console.log(data, "data");
+  console.log(users, "data");
 
-  return <div className="App">Salam</div>;
+  return (
+    <>
+      <Table columns={column} dataSource={users} />
+    </>
+  );
 }
 
 export default App;
--- src/api/apiSlice.ts
+++ src/api/apiSlice.ts
@@ -1,14 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import User from "../types/User";
 
 export const apiSlice = createApi({
-  reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
-  tagTypes: ["Users"],
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: () => "/users",
-      providesTags: ["Users"],
     }),
   }),
 });
